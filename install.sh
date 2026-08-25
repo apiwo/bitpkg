d9242c5cab33a091ff44db28dad09b8419076b2d
apiwo <rayingeneral@gmail.com>
2026-08-25 19:54:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 set -e
 
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Error: Root permissions needed." >&2
+    echo "Error: Root permissions needed. Run installer with root (e.g. doas ./install.sh or sudo ./install.sh)." >&2
     exit 1
 fi
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-chmod +x "$SCRIPT_DIR/pm"
+echo "==> Compiling pm with Go..."
+go build -o "$SCRIPT_DIR/pm" "$SCRIPT_DIR/main.go"
 
 "$SCRIPT_DIR/pm" c
 
